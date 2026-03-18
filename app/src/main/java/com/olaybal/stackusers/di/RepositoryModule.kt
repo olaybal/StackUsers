9028f32e60d7e7cf6b19d2a65bfa62f092baa54f
jom <>
2026-03-18 19:17:42 +0800

    feat(di): bind UserRepository implementation with Hilt

---
+++ app/src/main/java/com/olaybal/stackusers/di/RepositoryModule.kt
@@ -0,0 +1,20 @@
+package com.olaybal.stackusers.di
+
+import com.olaybal.stackusers.data.repository.UserRepositoryImpl
+import com.olaybal.stackusers.domain.repository.UserRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindUserRepository(
+        impl: UserRepositoryImpl
+    ): UserRepository
+}
